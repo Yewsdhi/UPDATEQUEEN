148fe108fb67fd7bbd7e077a1ca4251b58566170
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-07-10 19:33:22 +0530

    Update userbot.py

--- ishu/core/userbot.py
+++ ishu/core/userbot.py
@@ -59,7 +59,7 @@ class Userbot(Client):
         client.mention = ub.me.mention
         self.clients.append(client)
         try:
-            await ub.join_chat("AvyraUpdates")
+            await ub.join_chat("ll_ROYAL_ABOUT_ll")
         except Exception:
             pass
         logger.info(f"Assistant {num} started as @{client.username}")
